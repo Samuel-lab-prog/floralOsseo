b284a91b831fcc7ee064700c413b4c26df7598a9
Samuel <sg1976387@gmail.com>
2025-09-08 09:16:02 -0300

    conectado com o supabase

--- src/db/pool.js
+++ src/db/pool.js
@@ -9,11 +9,8 @@ dotenv.config({
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: false
 });
 
 module.exports = pool;
